164e3a7b1c543746aa3bdf23b0910658d9eab69f
ivanhercaz <ivan@ivanhercaz.com>
2020-05-09 21:32:22 +0100

    travis, mix: add coveralls config

--- mix.exs
+++ mix.exs
@@ -9,7 +9,14 @@ defmodule ExTldr.MixProject do
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      escript: [main_module: ExTldr, name: "extldr"]
+      escript: [main_module: ExTldr, name: "extldr"],
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -29,7 +36,8 @@ defmodule ExTldr.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 1.6"}
+      {:httpoison, "~> 1.6"},
+      {:excoveralls, "~> 0.12.3", only: :test}
     ]
   end
 end
